63d42f33818a49203cc19e4cdd797845a25c8609
Alisa Sofianova <alr2097@gmail.com>
2024-04-10 11:36:39 +0300

    scopes for filters

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Review;
@@ -12,5 +13,17 @@ class Book extends Model
 
     public function reviews(){
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder{
+        return $query->where('title', 'LIKE', '%'.$title.'%');
+    }
+
+    public function scopePopular(Builder $query): Builder{
+        return $query->whitCount('reviews')->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query):Builder{
+        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
     }
 }
